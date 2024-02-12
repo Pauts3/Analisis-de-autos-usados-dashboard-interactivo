da02c25504c951e6928ed0c2293668a0c0cb5e27
Paula Trujillo S <paulatrujisan@gmail.com>
2024-02-12 17:34:43 -0600

    Cambios en la gráfica interactiva.

--- app.py
+++ app.py
@@ -5,8 +5,6 @@ import numpy as np
 
 car_data = pd.read_csv('./notebooks/vehicles_us.csv')
 
-num_columns = ['price', 'model_year']
-
 obj_columns = ['model', 'condition', 'fuel', 'transmission', 'type']
 
 st.title('Wheel Wonders: Where Every Turn Leads to Your Perfect Pre-Loved Ride')
@@ -43,16 +41,11 @@ st.header('How Pre-loved are the Wheel Wonders')
 
 st.write("Check out the cars' condition by year")
 
-col1, col2 = st.columns([0.25, 0.75])
+col1, col2 = st.columns([0.15, 0.85])
 
 with col1:
-
-    x_axis = st.selectbox('X axis:', 'num_columns', index=0)
-
-    y_axis = st.selectbox('Y axis:', num_columns, index=1)
-
     c_axis = st.selectbox('Color:', obj_columns)
 
 with col2:
-    fig2, ax = st.bar_chart(car_data, x=x_axis, y=y_axis, color=c_axis)
+    fig2, ax = st.bar_chart(car_data, x='model_year', y='price', color=c_axis)
     st.plotly_chart(fig2)
