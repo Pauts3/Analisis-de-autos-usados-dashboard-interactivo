1acfa68aa4ae497fc54b705df8b71a321544fbba
Paula Trujillo S <paulatrujisan@gmail.com>
2024-02-12 19:50:55 -0600

    Arreglos en el código de la última gráfica.

--- app.py
+++ app.py
@@ -46,5 +46,6 @@ with col1:
     c_axis = st.selectbox('Color:', obj_columns)
 
 with col2:
-    fig3, ax = st.bar_chart(car_data, x='model_year', y='price', color=c_axis)
+    fig3, ax = st.bar_chart(car_data, x='model_year',
+                            y='price', color=c_axis, use_container_width=True)
     st.plotly_chart(fig3)
